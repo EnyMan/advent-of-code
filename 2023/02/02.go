72f149ad5ae441941b038b9bd6a19a74bffc4c12
EnyMan <Mous.imp@gmail.com>
2023-12-02 13:43:24 +0100

    Add day 2 solution

--- 2023/02/02.go
+++ 2023/02/02.go
@@ -1,5 +1,112 @@
 package second
 
-func Second() {
+import (
+	helpers "advent/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func violates_rule(trial_sample string, part2 bool) bool {
+	counts := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+	sample_splits := strings.Split(trial_sample, " ")
+	count, color := sample_splits[0], sample_splits[1]
+	countInt, err := strconv.Atoi(count)
+	if err != nil {
+		fmt.Println(err)
+		return true
+	}
+	if !part2 {
+		fmt.Println(countInt, color)
+	}
+	if countInt > counts[color] {
+		return true
+	}
+	return false
+}
+
+func calcualte_game_power(trials []string) int {
+	min_counts := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
+	}
+	for _, trial := range trials {
+		trial_splits := strings.Split(trial, ",")
+		for _, trial_split := range trial_splits {
+			trial_split := strings.TrimLeft(trial_split, " ")
+			sample_splits := strings.Split(trial_split, " ")
+			count, color := sample_splits[0], sample_splits[1]
+			countInt, err := strconv.Atoi(count)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if countInt > min_counts[color] {
+				min_counts[color] = countInt
+			}
+		}
+		fmt.Println(min_counts)
+	}
+	return min_counts["red"] * min_counts["green"] * min_counts["blue"]
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+func parse_input(input []string, part2 bool) []int {
+	ok_ids := []int{}
+	game_powers := []int{}
+	game_violation := bool(false)
+	for _, line := range input {
+		fmt.Println(line)
+		line_splits := strings.Split(line, ":")
+		header, game_data := line_splits[0], line_splits[1]
+		header_splits := strings.Split(header, " ")
+		game_id := header_splits[1]
+		trials := strings.Split(game_data, ";")
+		game_violation = false
+		game_power := calcualte_game_power(trials)
+		for _, trial := range trials {
+			trial_splits := strings.Split(trial, ",")
+			for _, trial_split := range trial_splits {
+				trial_split := strings.TrimLeft(trial_split, " ")
+				if violates_rule(trial_split, part2) {
+					game_violation = true
+				}
+				if game_violation {
+					break
+				}
+			}
+			if game_violation && !part2 {
+				break
+			}
+		}
+		game_idInt, err := strconv.Atoi(game_id)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		if !game_violation {
+			ok_ids = append(ok_ids, game_idInt)
+		}
+		game_powers = append(game_powers, game_power)
+	}
+	if part2 {
+		return game_powers
+	} else {
+		return ok_ids
+	}
+}
+
+func Second(filename string, part2 bool) {
+	lines := helpers.ReadFile(filename)
+	results := parse_input(lines, part2)
+	sum := 0
+	for _, result := range results {
+		sum += result
+	}
+	fmt.Println(sum)
 }
